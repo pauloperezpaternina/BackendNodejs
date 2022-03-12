89fdb827c444cd731a4120b46de55300bd6db1b5
Paulo Perez <pauloperez@bittronic.net>
2022-03-11 22:45:25 -0500

    Message 2 try multihilo

---
+++ Fundamentos/monohilo.js
@@ -0,0 +1,15 @@
+console.log('Hi Everyone!');
+
+let i = 0;
+
+setInterval(function() {
+    console.log(i);
+    i++;
+
+    /*if (i===5){
+        var a = 3 + z;
+    }*/
+
+}, 1000);
+
+console.log('Message 2');
