54ccff68919ab8e4a4ff0f950f88a3fb4d5ed50d
Paulo Perez <pauloperez@bittronic.net>
2022-04-06 09:58:27 -0500

    Created model users

---
+++ API-Nodejs/models/nosql/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const UserScheme = new mongoose.Schema(
+    {
+        name: {type : String},
+        age: {type:Number},
+        email: {type:String, unique:true},
+        password: {type: String},
+        role: {type:['user','admin'], default: 'user'},
+
+    }, 
+    {
+        timestamps:true,
+        versionkey:false
+    }
+);
+
+module.exports = mongoose.model("users", UserSheme)
