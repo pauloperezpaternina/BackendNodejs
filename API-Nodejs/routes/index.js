58b46bcb53371046935f686128c2a44c3d76dbe0
Paulo Perez <pauloperez@bittronic.net>
2022-04-18 22:29:03 -0500

    path_route created

--- API-Nodejs/routes/index.js
+++ API-Nodejs/routes/index.js
@@ -3,9 +3,18 @@ const fs =require("fs");
 const router = express.Router();
 
 const PATH_ROUTES = __dirname;
+const removeExt = (filename) =>{
+    return filename.split('.').shift()
+}
 
-const a = fs.readdirSync(PATH_ROUTES)
+const a = fs.readdirSync(PATH_ROUTES).filter((file) => {
+    const name = removeExt(file)
+    if(name !== 'index'){
+        router.use(`/${name}`, require(`./${file}`))
+    }
 
-console.log({a})
+})
+
+//console.log({a})
 
 module.exports = router
--- API-Nodejs/routes/tracks.js
+++ API-Nodejs/routes/tracks.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 
+/**
+ * http://localhost/tracks  GET, POST, DELETE, PUT
+ * 
+ */
 
 router.get("/", (req, res) => {
 
